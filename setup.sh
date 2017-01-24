b0e8d3ecbf7815cd9c341f2e5632710a9593664f
Albert <noe.thalheim@gmail.com>
2017-01-24 18:25:16 +0100

    Add bashrc to dotfiles

--- setup.sh
+++ setup.sh
@@ -7,4 +7,8 @@ pwd=$(pwd)
 ln -s ${pwd}/vimrc ~/.vimrc
 ln -s ${pwd}/tmux.conf ~/.tmux.conf
 
+echo "Add bashrc to ~/.profile"
+echo "source $HOME/.bashrc" >> ~/.profile
+ln -s ${pwd}/bashrc ~/.bashrc 
+
 echo "Done"
